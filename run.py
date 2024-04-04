f3800a183875a482f8d46190ad6beda4fc4145ce
Leon Potgieter <hexvallei@gmail.com>
2024-04-04 10:11:22 +0000

    add google sheets credentials and check data retrieval

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+##############################################################################
+import gspread # Google Sheet Data
+from google.oauth2.service_account import Credentials # Google Sheets Access
+from pprint import pprint # Temp, for debugging
+import os # For terminal clearing
+
+# Google Sheet Credentials and SHEET Variable
+SCOPE = ["https://www.googleapis.com/auth/spreadsheets",
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('vault_guard_db')
+
+os.system("clear")
+
+data = SHEET.worksheet('test')
+pprint(data.get_all_values())
