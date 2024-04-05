ba7faffc1a86f3f5740a97aef931739ed467300a
Leon Potgieter <hexvallei@gmail.com>
2024-04-05 15:40:38 +0000

    update docstring text and add more comments throught Python code

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
-import gspread  # Google Sheet Data
-from google.oauth2.service_account import Credentials  # Google Sheets Access
+
 import os  # For terminal clearing
-from consolemenu import *  # Menu Generation
-from consolemenu.items import *  # Menu Item Generation
-from colorama import Fore, Back, Style, init  # Terminal Text Colours
 import time  # for delays
 import sys  # For writing to terminal with effects
-import getch  # Capture and record Keypresses
-import hashlib  # For Hashing Login Passwords of Users (for this app)
-import re  # For Checking password strength with Regular Expressions
+import gspread  # Google Sheet Data
+from google.oauth2.service_account import Credentials  # Google Sheets Access
+from consolemenu import *  # Menu Generation
+from consolemenu.items import *  # Menu Item Generation
 from prettytable import PrettyTable  # Table Display
-import re  # For generating random passwords
+from colorama import Fore, Back, Style, init  # Terminal Text Colours
+import getch  # Capture and record Keypresses
+import hashlib  # For Hashing Login Passwords of Users
+import re  # For Checking password strength with Regular Expressions
 import random  # For generating random passwords
 
 # Imports below are for interacting with the 'Have I Been Pwned' API
@@ -25,7 +25,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 
-# Google Sheet Access Credentials
+# Google Sheet Credentials
 SCOPE = ["https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file",
          "https://www.googleapis.com/auth/drive"]
@@ -33,11 +33,13 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-# Initialise Users Information from Google Sheets
+# Initialise current list of users Google Sheets
 SHEET = GSPREAD_CLIENT.open('vault_guard_db')
 user_data = SHEET.worksheet('users')
 login_usernames = user_data.col_values(1)
 login_passwords = user_data.col_values(2)
+
+# Initialise global variables for password encryption
 current_user = None
 key_source = None
 
@@ -47,6 +49,10 @@ def clear_screen():
 
 
 def typewr(text):
+    '''
+    This function types text to the terminal with a delay, creating a
+    typewriter effect.
+    '''
     text_arr = [char for char in text]
     for i in text_arr:
         sys.stdout.write(i)
@@ -56,8 +62,8 @@ def typewr(text):
 
 def reorder_list_ids():
     '''
-    This function Reorders the IDs of a user database, after an item has
-    been deleted.
+    This function Reorders the IDs of the users database, after an item has
+    been deleted. This is done to avoid future duplicated IDs.
     '''
     for i in range(1, len(current_user.col_values(1)[1:])+1):
         current_user.update_cell(i+1, 1, i)
@@ -65,7 +71,7 @@ def reorder_list_ids():
 
 def generate_password():
     '''
-    Generate a strong random password for user
+    Generates a strong random password for user
     '''
     while True:
         length = input(('\nPlease Enter Generated Password Length'
@@ -76,6 +82,7 @@ def generate_password():
             length = int(length)
             break
 
+    # Strong passwords should include at least 1 of each in p1 - p5
     p1 = [i for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
     p2 = [i for i in 'abcdefghijklmnopqrstuvwxyz']
     p3 = [i for i in '0123456789']
@@ -88,8 +95,9 @@ def generate_password():
             item = random.choice(p5)
             new_password += random.choice(item)
 
+        # Check if the generated password is indeed strong, if not: Repeat
         strong_pass = []
-        # strong_pass.append(len(new_password) >= 8)
+        strong_pass.append(len(new_password) >= 8)
         strong_pass.append(bool(re.search("[A-Z]", new_password)))
         strong_pass.append(bool(re.search("[a-z]", new_password)))
         strong_pass.append(bool(re.search("[0-9]", new_password)))
@@ -141,6 +149,7 @@ def view_vault():
     hide_pass_display = []
     show_pass_display = []
 
+    # Decrypted passwords are temporarily stored in show_pass_display
     for password in gsheet_passwords:
         hide_pass_display.append('********')
         show_pass_display.append(decrypt_password(password))
@@ -167,6 +176,7 @@ def view_vault():
         print('Press ' + Fore.YELLOW + 'Enter'
                        + Style.RESET_ALL + ' to return to the main menu')
 
+        # Give users the option to display passwords with Spacebar
         key = getch.getch()
         if key == '\n':
             break
@@ -177,16 +187,19 @@ def view_vault():
 def add_to_vault():
     '''
     Allows user to add new stored credentials to vault, includes optional
-    STRONG password generator that the user may use.
+    strong password generator that the user may use.
     '''
     print('Your Current Vault:\n')
     view_vault_read_only()
+
+    # Generates new ID of 1, if the user vault is empty
     try:
         add_id = str(int(SHEET.worksheet(current_user.title)
                          .col_values(1)[-1])+1)
     except Exception:
         add_id = 1
 
+    # The 'while True' loops below enforce strong data handling
     while True:
         add_service = input('\nPlease Enter New Service: \n')
         if not (re.findall(r"\w", add_service)):
@@ -227,8 +240,8 @@ def add_to_vault():
 
 def edit_vault_item():
     '''
-    Allows user to edit existing credentials in vault. Includes optional STRONG
-    password generator the user may use.
+    Allows user to edit existing credentials in vault. Includes optional strong
+    password generator the user may use when editing items.
     '''
     gsheet_ids = SHEET.worksheet(current_user.title).col_values(1)[1:]
     print('Your Current Vault:\n')
@@ -242,6 +255,8 @@ def edit_vault_item():
         return
 
     print('\n')
+
+    # The 'while True' loops below enforce strong data handling
     while True:
         to_edit = input('Please Enter the ID (in digits) of the vault item'
                         ' you would like to edit: \n')
@@ -289,6 +304,7 @@ def edit_vault_item():
         else:
             break
 
+    # Update existing items in place on Google Sheet
     to_edit = int(to_edit)+1
     current_user.update_cell(to_edit, 2, add_service)
     current_user.update_cell(to_edit, 3, add_username)
@@ -302,7 +318,7 @@ def edit_vault_item():
 def delete_from_vault():
     '''
     Allows user to remove exsiting credential from vault. Requires confirmation
-    before deletion. Updates Credential ID's (on GSheet database) after
+    before deletion. Updates Credential ID's (on Google Sheet database) after
     item deletion.
     '''
     gsheet_ids = SHEET.worksheet(current_user.title).col_values(1)[1:]
@@ -317,6 +333,8 @@ def delete_from_vault():
         return
 
     print('\n')
+
+    # The 'while True' loop below enforce strong data handling
     while True:
         to_del = input('Please Enter the ID (in digits) of the vault item '
                        'you would like to delete: \n')
@@ -333,6 +351,7 @@ def delete_from_vault():
         else:
             break
 
+    # Confirm deletion from user before proceeding
     x = int(to_del)
     row_to_del = SHEET.worksheet(current_user.title).get_all_values()[x]
     print('\n' + Back.RED + 'Are you sure you want to delete the following '
@@ -345,6 +364,8 @@ def delete_from_vault():
     print('\n' + Fore.RED + ' ** Press y to delete ** ' + Style.RESET_ALL)
     print('Press any other key to return to the main menu')
     key = getch.getch()
+
+    # Delete item (row) from Google Sheet
     if key == 'y' or key == 'Y':
         SHEET.worksheet(current_user.title).delete_rows(int(to_del)+1)
         reorder_list_ids()
@@ -357,7 +378,7 @@ def delete_from_vault():
 def check_leaks():
     '''
     Check for password breaches using the free API from haveibeenpwned.com
-    User Vault passwords are checked again the regularly updated database at
+    User Vault passwords are checked against the regularly updated database at
     haveibeenpwned.com
     '''
     typewr('The list of passwords in your vault '
@@ -395,6 +416,7 @@ def check_leaks():
     table.field_names = ["Entry ID", "Service", "Username", "Password",
                          "Data Breaches"]
 
+    # The pyhibp library simplifies the API call to haveibeenpwned.com
     pyhibp.set_user_agent(ua="VaultGuard v1.0 (Code Institute Test Project)")
 
     any_passwords_breached = False
@@ -406,6 +428,11 @@ def check_leaks():
             test_data[i].append(breached)
             table.add_row(test_data[i])
 
+    '''
+    Notify users of breached passwords, displaying passwords + number
+    of data breaches in which the password was revealed. Otherwise
+    confirm that no passwords have been revealed.
+    '''
     if any_passwords_breached:
         print('\nThe following passwords were found to be compromised\n')
         print(table)
@@ -444,7 +471,8 @@ def display_login_menu():
 def encrypt_password(p):
     '''
     Uses SHA256 algorith to encrypt user passwords before writing them to the
-    Gsheet database.
+    Google Sheet database. This function uses the built in
+    Python Cryptography library.
     '''
     salt = bytes(key_source.encode('utf-8'))
     kdf = PBKDF2HMAC(
@@ -463,8 +491,9 @@ def encrypt_password(p):
 
 def decrypt_password(p):
     '''
-    Uses SHA256 algorith to decrypt user passwords from the Gsheet database
-    to display them in plaintext within the app.
+    Uses SHA256 algorith to decrypt user passwords from the Google Sheet
+    database to display them in plaintext within the app. This function
+    uses the built in Python Cryptography library.
     '''
     salt = bytes(key_source.encode('utf-8'))
     kdf = PBKDF2HMAC(
@@ -477,23 +506,30 @@ def decrypt_password(p):
     key = base64.urlsafe_b64encode(kdf.derive(bytes(
                                                 key_source.encode('utf-8'))))
     FKEY = Fernet(key)
-    # ** Encrypt and return password using Fernet Key **
+    # ** Decrypt and return password using Fernet Key **
     return FKEY.decrypt(p).decode()
 
 
 def correct_password(user_id, user_password):
+    '''
+    Used during login, this function compares a hash of the user entered
+    password, against the hash of the previously saved password on Google
+    Sheets.
+    '''
     up_en = hashlib.sha256(bytes(user_password.encode('utf-8'))).hexdigest()
     return (up_en == login_passwords[user_id])
 
 
 def login():
     '''
-    Allows user to login. Checks for data handling errors.
+    Allows user to login, by comparing username and password to stored
+    information on Google Sheet Database.
     '''
     while True:
         clear_screen()
 
         user_name = input("Please enter your username: \n")
+        # Data Handling for incorrect username
         if user_name == '' or user_name == ' ' or len(user_name) < 3:
             print('\n')
             print(Back.RED + 'You entered an invalid username,'
@@ -501,7 +537,7 @@ def login():
             print('         Press any key to try again')
             key = getch.getch()
             continue
-
+        # Data Handling for absent username
         elif user_name not in login_usernames:
             print('\n')
             print(Back.RED + ' Username not found. Do you have an account? '
@@ -549,8 +585,8 @@ def login():
 
 def strong_password(password, password2):
     '''
-    Check the strength of a new users password and displays info allowing user
-    to correct if a password is too weak.
+    Check the strength of a new user's password and displays password
+    info allowing user to make corrections if a password is too weak.
     '''
     # 1: Length
     # 2: Uppercase
@@ -615,10 +651,11 @@ def strong_password(password, password2):
 
 def create_new_account():
     '''
-    Allows user to create a new account. Checks for valid username and STRONG
-    password. If successfull, user credentials are stored in GSheet Database
-    (with the user password being Hashed for future comparison). A new user
-    database is then openened (a new GSheet Worksheet) to store user items.
+    Allows user to create a new account. Checks for valid username and strong
+    password. If successfull, user credentials are stored in the Google Sheet
+    Database (with the user password being hashed for future comparison).
+    A new user database is then openened (a new Google Sheet Worksheet)
+    to store user items. This function has strong data handling.
     '''
     global SHEET
     global user_data
@@ -715,6 +752,7 @@ def display_main_menu():
     '''
     Welcome screen to greet users upon app opening.
     '''
+    # Loads ASCII Art Logo to display in main menu.
     f = open('banner.txt', 'r')
     lines = f.read()
     f.close()
